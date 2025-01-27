185b0c28febe2eed4185021c6e9603d0e5b19a9f
Titoputraaa <titobamulo13@gmail.com>
2025-01-27 22:48:03 +0700

    make method input

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
+    static String[] arr_value;
+    
     public static void main(String[] args) {
         System.out.println("\nWELCOME TO RENDOM WHEEL");
         do {
@@ -9,19 +12,19 @@ public class Main {
             System.out.println("2 : SPIN ");
             System.out.println("3 : HISTORY ");
             System.out.println("4 : EXIT ");
-            System.out.println("PICK : ");
+            System.out.print("PICK : ");
             int pick = sc.nextInt();
-
+            
             switch (pick) {
                 case 1:
-                    
+                arr_value = input();
                     break;
                 case 2:
-                    
-                    break;
+                    System.out.println(Arrays.toString(arr_value));
+                        break;
                 case 3:
                     
-                    break;
+                        break;
                 case 4:
                     System.out.println("THANKYOU ;)");
                     return;
@@ -31,5 +34,18 @@ public class Main {
                     break;
             }
         } while (true);
+    }
+
+    // input value method 
+    static String[] input() {
+        System.out.print("input how much name : ");
+        int total_value = sc.nextInt();
+        String[] value = new String[total_value];
+        for (int i = 0; i < value.length; i++) {
+            System.out.print("input " + (i+1) + " : ");
+            value[i] = sc.next();
+        }
+        System.out.println(Arrays.toString(value));
+        return value;
     }
 }
