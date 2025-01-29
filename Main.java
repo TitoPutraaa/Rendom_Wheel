92996dbcaf22581601956eb2a832f5624905777c
Titoputraaa <titobamulo13@gmail.com>
2025-01-29 21:03:03 +0700

    yuk bisa yukk

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     // input value method 
     static String[] input() {
-        System.out.print("input how much name : ");
+        System.out.print("input how much name : "); 
         int total_value = sc.nextInt();
         String[] value = new String[total_value];
         for (int i = 0; i < value.length; i++) {
@@ -18,40 +18,7 @@ public class Main {
     }
 
     static void spin() {
-        int rendom ;
-        System.out.println("---------------------");
-        System.out.println("        " + arr_value[rendom]);
-        System.out.println("---------------------");
-        System.out.println("\n1 : remove");
-        System.out.println("2 : close");
-        System.out.print("pick : ");
-        int pick = sc.nextInt();
         
-        switch (pick) {
-            case 1:
-            boolean re = true;
-            for (int x = 0; x < arr_value.length; x++) {
-                    rendom = (int)(Math.random() * arr_value.length);
-                        arr_value[rendom] = "";
-                        if (rendom < arr_value.length - (x+1) && arr_value[rendom] != "") {
-                            for (int i = 0; i < arr_value.length; i++) {
-                                if (arr_value[i] == "" && i != arr_value.length-1) {
-                                    String mov = arr_value[i];
-                                    arr_value[i] = arr_value[i+1];
-                                    arr_value[i+1] = mov;
-                                }
-                            }
-                        }
-                }
-                    break;
-                    case 2:
-                    System.out.println(Arrays.toString(arr_value));
-                    return;
-            
-                    default:
-                    System.out.println("EROR! || pick either 1 or 2");
-                    break;
-            }
     }
 
     public static void main(String[] args) {
@@ -70,8 +37,7 @@ public class Main {
                     arr_value = input();
                 break;
                 case 2:
-                    spin();
-                    System.out.println(Arrays.toString(arr_value));
+
                 break;
                 case 3:
                 
